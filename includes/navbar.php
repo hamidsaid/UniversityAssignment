86b9a5c807851ded6432e5847225b567b390fc98
hamidsaid <63158771+hamidsaid@users.noreply.github.com>
2021-08-02 16:30:16 +0300

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>IS181</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+           <!-- Google Fonts -->
+         <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu" rel="stylesheet">    
+         <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+         
+         <!-- CSS Stylesheets -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      
+      <link href="css/styles.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+
+       <!-- Bootstrap Scripts -->
+       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+     
+<section class="nav-section">
+      <div class="container-fluid nav-div">
+      <!-- Nav Bar -->
+
+      <nav class="navbar navbar-expand-lg navbar-light" >
+
+        <a class="navbar-brand" href="">HAMID SAID</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="courses.php">Courses</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cv/Hamids_resume.pdf" download>CV</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contacts.php">Contacts</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </div>
+  </section>
+  
